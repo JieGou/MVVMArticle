82332a461e04c2c0d9460545d970fa70348d3d89
陈杰 <jiegou04@gmail.com>
2020-03-21 19:51:59 +0800

    no Change。Format Modified

--- RssModel/FeedReader.cs
+++ RssModel/FeedReader.cs
@@ -57,14 +57,19 @@ namespace RssModel
                 return null;
             }
 
-            Feed feed = new Feed(channelNode["title"].InnerText, channelNode["description"].InnerText, new Uri(channelNode["link"].InnerText));
+            Feed feed = new Feed(channelNode["title"].InnerText,
+                channelNode["description"].InnerText,
+                new Uri(channelNode["link"].InnerText));
 
             for (int i = 0; i < channelNode.ChildNodes.Count; ++i)
             {
                 XmlNode itemNode = channelNode.ChildNodes[i];
                 if (itemNode.Name == "item")
                 {
-                    FeedItem item = new FeedItem(itemNode["title"].InnerText, itemNode["description"].InnerText, new Uri(itemNode["link"].InnerText));
+                    FeedItem item = new FeedItem(itemNode["title"].InnerText,
+                        itemNode["description"].InnerText,
+                        new Uri(itemNode["link"].InnerText));
+
                     feed.Items.Add(item);
                 }
             }
